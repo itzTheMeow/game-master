749d81f4b3a49a5864d9077a3af9342107232fa2
Meow <erty2pop@gmail.com>
2020-04-23 23:00:21 -0400

    add chat filter

---
+++ modules/chatFilter.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js")
+
+module.exports = async (bot, message) => {
+    if(message.channel.id !== "702621363201900624") return;
+    let hook = new Discord.WebhookClient("703077206271852615", "fObzB6J-qhAsAzrkEjk0NbqWGNZx-_tVh-9y3z-xDXdbEj9IG3T9R-AaiFz9LjHr_kG9")
+
+    let content = message.content
+    bot.config.badWords.forEach(word => {
+        let censor = []
+        while(censor.length !== word.length) {
+            censor.push("\\*")
+        }
+        eval(`content.replace(/${word}/g, ${censor})`)
+    })
+
+    await hook.edit({name: message.member.nickname, avatar: message.author.displayAvatarURL})
+    await hook.send(content)
+    await hook.edit({name: "CHAT WEBHOOK", avatar: message.guild.iconURL})
+}
